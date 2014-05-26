e2d103bfcf3e11ae7ce2ab9dcfe25242e76c48bc
Robert Zaremba <robert.zaremba@zoho.com>
2014-05-26 17:26:31 +0200

    Added docs files

---
+++ godoc.go
@@ -0,0 +1,9 @@
+// This package is an extension to github.com/go-check/check library.
+// It provides additional usefull checkers:
+// * Contains (check if a slice/array/string contains specified element)
+// * EqualsWithTolerance - check if two numbers are "close enough"
+// * IsTrue
+// * IsEmpty - check if specified object is empty (nil, [], {}, "", 0)
+// * SlicesEquals - check if 2 slices contain the same elements
+
+package checkers
